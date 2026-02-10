14d1637bd2832cbe1e25ab21fe299acd00dc277f
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2026-02-10 22:11:59 +0100

    Improve a little bit the console example

--- examples/Perplexity.Examples.Console/Program.cs
+++ examples/Perplexity.Examples.Console/Program.cs
@@ -3,6 +3,7 @@ using Perplexity.Chat.Dtos;
 using Perplexity.Exceptions;
 
 Console.WriteLine("Perplexity Console Example");
+Console.WriteLine();
 var apikey = Environment.GetEnvironmentVariable("PERPLEXITY_APIKEY") ?? throw new PerplexityMissingApiKeyException();
 var perplexityClient = new PerplexityClient(apikey);
 var chatClient = perplexityClient.ChatClient;
@@ -15,4 +16,14 @@ var request = new CreateChatCompletionRequest
     ]
 };
 var response = await chatClient.CreateChatCompletion(request);
-Console.WriteLine(response.Choices[0].Message.Content);
+Console.WriteLine("MESSAGES");
+foreach (var choice in response.Choices)
+{
+    Console.WriteLine(choice.Message.Content);
+}
+Console.WriteLine();
+Console.WriteLine("CITATIONS");
+foreach (var citation in response.Citations)
+{
+    Console.WriteLine(citation);
+}
