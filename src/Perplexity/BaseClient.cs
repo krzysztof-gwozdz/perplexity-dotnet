daa4e95a48304e0dbeb8a63a06a208277fff96b0
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2026-02-10 14:15:50 +0100

    Ignore nulls when sending request to api

--- src/Perplexity/BaseClient.cs
+++ src/Perplexity/BaseClient.cs
@@ -1,10 +1,16 @@
 using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Perplexity;
 
 public abstract class BaseClient(HttpClient httpClient)
 {
+    private readonly JsonSerializerOptions _options = new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+    };
+
     protected async Task<TResponse> Get<TResponse>(string requestUri, CancellationToken cancellationToken)
     {
         var response = await httpClient.GetAsync(requestUri, cancellationToken: cancellationToken);
@@ -12,16 +18,16 @@ public abstract class BaseClient(HttpClient httpClient)
         response.EnsureSuccessStatusCode();
         return ParseResponse<TResponse>(content);
     }
-    
+
     protected async Task Post<TRequest>(string requestUri, TRequest value, CancellationToken cancellationToken)
     {
-        var response = await httpClient.PostAsJsonAsync(requestUri, value, cancellationToken: cancellationToken);
+        var response = await httpClient.PostAsJsonAsync(requestUri, value, _options, cancellationToken: cancellationToken);
         response.EnsureSuccessStatusCode();
     }
-    
+
     protected async Task<TResponse> Post<TRequest, TResponse>(string requestUri, TRequest value, CancellationToken cancellationToken)
     {
-        var response = await httpClient.PostAsJsonAsync(requestUri, value, cancellationToken: cancellationToken);
+        var response = await httpClient.PostAsJsonAsync(requestUri, value, _options, cancellationToken: cancellationToken);
         var content = await response.Content.ReadAsStringAsync(cancellationToken);
         response.EnsureSuccessStatusCode();
         return ParseResponse<TResponse>(content);
